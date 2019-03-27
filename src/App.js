7d2a0c65b6f3f55de79e7456bf656d700f0ff0c7
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-03-27 12:34:47 -0700

    changes added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
 import React, { Component } from 'react';
-class Hello extends Component {
+
+import Comment from './Comment.js';
+
+class Post extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, {this.props.name}!</h1>
-        <p>You are {this.props.age} years old.</p>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.author}</p>
+        <div>
+          <p>{this.props.body}</p>
+        </div>
+        <h3>Comments:</h3>
+        <Comment message={this.props.comments[0]} />
       </div>
     );
   }
 };
 
-export default Hello;
+export default Post;
